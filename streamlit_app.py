cf71e148010ddfcaf39b09be9137d37f04a6f85f
nickbaker-tahola <99820048+nickbaker-tahola@users.noreply.github.com>
2022-12-13 17:14:59 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,3 +18,6 @@ df = pandas.DataFrame(my_catalog)
 # temp write the dataframe to the page so I Can see what I am working with
 streamlit.write(df)
 
+# put the first column into a list
+color_list = df[0].values.tolist()
+print(color_list)
